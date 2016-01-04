c267201c2abb76652ea4c7cb5132664b64fc96eb
Jonathan Dunlap <jdunlap@outlook.com>
2016-01-03 21:23:00 -0800

    removed node-lambda + setTable added

--- src/index.js
+++ src/index.js
@@ -12,22 +12,30 @@ function rxify(db) {
     findDoc: (table, ...args) => rxo.fromNodeCallback(db[table].findDoc, db[table])(...args),
     where: (table, ...args) => rxo.fromNodeCallback(db[table].where, db[table])(...args),
     findOne: (table, ...args) => rxo.fromNodeCallback(db[table].findOne, db[table])(...args),
-    save: (table, ...args) => rxo.fromNodeCallback(db[table].save, db[table])(...args)
+    save: (table, ...args) => rxo.fromNodeCallback(db[table].save, db[table])(...args),
+    setTable: table => ({
+      destroy: rxo.fromNodeCallback(db[table].destroy, db[table]),
+      searchDoc: rxo.fromNodeCallback(db[table].searchDoc, db[table]),
+      find: rxo.fromNodeCallback(db[table].find, db[table]),
+      findDoc: rxo.fromNodeCallback(db[table].findDoc, db[table]),
+      where: rxo.fromNodeCallback(db[table].where, db[table])(...args),
+      findOne: rxo.fromNodeCallback(db[table].findOne, db[table]),
+      save: rxo.fromNodeCallback(db[table].save, db[table])
+    })
   }
 }
 
 export function connectSync(opt) {
-  const db = massive.connectSync(opt); //db:'reportal', 
+  const db = massive.connectSync(opt);
   return new rxify(db);
 }
 
 export function connect(opt) {
   const connect = rxo.fromNodeCallback(massive.connect, massive)
-  const db = connect(opt).map(rxify); //db:'reportal', 
-  return db;
+  return connect(opt).map(rxify);
 }
 
 export function connectMassive(db) {
-  if(db) return new rxify(db);
-  else throw new Error('db parameter not set');
+  if(!db) throw new Error('db parameter not set');
+  return new rxify(db);
 }
